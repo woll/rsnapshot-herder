301c2271299152bac30dde8457606fed442fe6db
woll <github@2-islands.com>
2025-06-10 18:42:28 +0100

    Added restrict_commands.sh utility script

---
+++ restrict_commands.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -- junk $SSH_ORIGINAL_COMMAND
+shift
+case $1 in
+    sleep)
+        /bin/sleep 10
+        ;;
+    backup)
+	shift
+        sudo <path_to_install_dir>/rsnapshot-herder $*
+        ;;
+    *)
+        echo "Command not supported with this ssh key"
+        exit 1
+        ;;
+esac
